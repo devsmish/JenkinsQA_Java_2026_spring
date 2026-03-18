12b2c6f8bf1c1248f8fc315e3c6eb5e7338d79d9
olgasuperstars <olgasuperstar@mail.ru>
2026-03-18 19:11:54 +0100

    Test using the gismeteo website

---
+++ src/test/java/kuKuMojMalchikTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class kuKuMojMalchikTest {
+    @Test
+    public void testGismeteo() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.gismeteo.ru/");
+        WebElement button = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div[2]/button[1]/p"));
+        button.click();
+
+        WebElement nameCity = driver.findElement(By.xpath("//a[contains(text(), 'Москва')]"));
+        nameCity.click();
+        WebElement text = driver.findElement(By.xpath("//h1"));
+        Assert.assertEquals(text.getText(), "Погода в Москве");
+        driver.quit();
+
+    }
+}
