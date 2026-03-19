b17bfa3c59e6a7a8c68b82da5249430a91dea66d
Natalia <natysechka.markova@gmail.com>
2026-03-19 14:26:00 -0400

    Add one test

---
+++ src/test/java/NMTest1.java
@@ -0,0 +1,28 @@
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class NMTest1 {
+
+    @Test
+    public void testW3schools() {
+      WebDriver driver = new ChromeDriver();
+       //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+       driver.get("https://www.w3schools.com");
+
+        driver.findElement(By.id("search2")).sendKeys("Java");
+        driver.findElement(By.id("learntocode_searchbtn")).click();
+
+        WebElement text = driver.findElement(By.className("with-bookmark"));
+
+        Assert.assertEquals(text.getText(), "JavaScript Tutorial");
+
+            driver.quit();
+        }
+    }
+
