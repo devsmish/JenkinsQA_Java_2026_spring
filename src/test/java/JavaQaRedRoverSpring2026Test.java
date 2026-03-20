8f09d9f0dfa113a31255d3bcd715d833bcd0299b
viktor <tuzovviktor@gmail.com>
2026-03-20 23:54:31 +0500

    Добавил первый тест МТС

--- src/test/java/JavaQaRedRoverSpring2026Test.java
+++ src/test/java/JavaQaRedRoverSpring2026Test.java
@@ -29,4 +29,32 @@ public class JavaQaRedRoverSpring2026Test {
 
         driver.quit();
     }
+
+    @Test
+    public void testMtsBank() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.mtsbank.ru/");
+
+        WebElement numberBox = driver.findElement(By.xpath("//form//input"));
+        WebElement submitButton = driver.findElement(By.xpath("//form//button"));
+        WebElement textButton = driver.findElement(By.xpath("//form//button//div[@data-testid='text']"));
+
+        Assert.assertEquals(numberBox.getAttribute("placeholder"), "+7 000 000-00-00");
+        Assert.assertEquals(textButton.getText(), "Узнать");
+
+        numberBox.sendKeys("9173759423");
+        submitButton.click();
+
+        WebElement textForm = driver.findElement(By.xpath("(//div[@data-testid='heading'])[last()]"));
+        try {
+            Thread.sleep(1000);//пауза 1 сек. пока так
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assert.assertEquals(textForm.getText(), "Введите код подтверждения");
+
+        driver.quit();
+    }
 }
