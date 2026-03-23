c115e0d4dfc56b8e3b687aaeedab1291cccc45fb
buksha-evgenia <134475777+buksha-evgenia@users.noreply.github.com>
2026-03-23 19:00:46 +0700

    First commit to project (#59)
    GroupCosmosSociofobusTest with testAkvaZoomarket

---
+++ src/test/java/GroupCosmosSociofobusTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GroupCosmosSociofobusTest {
+
+    @Test
+    public void testAkvaZoomarket() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://akva-zoomarket.ru/");
+
+        WebElement textBox = driver.findElement(By.cssSelector("#title-search-input"));
+        WebElement button = driver.findElement(By.cssSelector("#title-search > form > button"));
+
+        textBox.sendKeys("undefined");
+        button.click();
+
+        WebElement message = driver.findElement(By.xpath("/html/body/div[1]/main/div[2]"));
+
+        Assert.assertEquals(message.getText(), "Сожалеем, но ничего не найдено.");
+
+        driver.quit();
+    }
+}
