bf63dfdb0184271bf593c7728daa2fd88a798e36
ArtAero <aero.minor.art@gmail.com>
2026-03-23 10:37:38 +0500

    new commit test attempt

---
+++ src/test/java/ArtAeroTest.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class ArtAeroTest {
+    @Test
+    public void sasflixTest(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://sasflix.ru/");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement searchBox = driver.findElement(By.xpath("//a[@href=\"/chat\"]"));
+        searchBox.click();
+
+        WebElement searchButton = driver.findElement(By.xpath("//div[@class=\"mt-3\"]/button[@class=\"btn btn-md btn-primary\" and @type=\"button\"]"));
+        searchButton.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement buttonLoginn = driver.findElement(By.xpath("//*[@id=\"BootstrapVueNext__ID__v-22__tab___\"]"));
+        buttonLoginn.click();
+
+        Assert.assertEquals(buttonLoginn.getText(), "Регистрация");
+
+        driver.quit();
+
+
+
+    }
+}
