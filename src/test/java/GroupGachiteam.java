9f2a5947842a2e4396ce8268c04ca3134a4ea614
Yerkezhan <m.erkosya@gmail.com>
2026-03-26 23:10:29 +0500

    updated with try final

--- src/test/java/GroupGachiteam.java
+++ src/test/java/GroupGachiteam.java
@@ -12,28 +12,32 @@ public class GroupGachiteam {
     @Test
     public void testCheckTitlesNameAndNavigation() {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://commitquality.com");
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
-        WebElement Product = driver.findElement(By.xpath("//*[@data-testid = 'navbar-products']"));
-        Assert.assertEquals("Products", Product.getText());
-        String valueOfActiveElement = driver.findElement(By.xpath("//*[@class='nav-link active']")).getAttribute("class");
-        Assert.assertTrue(valueOfActiveElement.contains("nav-link active"));
+        try {
+            driver.get("https://commitquality.com");
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
-        WebElement AddProduct = driver.findElement(By.xpath("//*[@data-testid = 'navbar-addproduct']"));
-        Assert.assertEquals("Add Product", AddProduct.getText());
-        AddProduct.click();
-        Assert.assertTrue(valueOfActiveElement.contains("nav-link active"));
+            WebElement Product = driver.findElement(By.xpath("//*[@data-testid = 'navbar-products']"));
+            Assert.assertEquals("Products", Product.getText());
+            String valueOfActiveElement = driver.findElement(By.xpath("//*[@class='nav-link active']")).getAttribute("class");
+            Assert.assertTrue(valueOfActiveElement.contains("nav-link active"));
 
-        WebElement Practice = driver.findElement(By.xpath("//*[@data-testid = 'navbar-practice']"));
-        Assert.assertEquals("Practice", Practice.getText());
-        Practice.click();
-        Assert.assertTrue(valueOfActiveElement.contains("nav-link active"));
+            WebElement AddProduct = driver.findElement(By.xpath("//*[@data-testid = 'navbar-addproduct']"));
+            Assert.assertEquals("Add Product", AddProduct.getText());
+            AddProduct.click();
+            Assert.assertTrue(valueOfActiveElement.contains("nav-link active"));
 
-        WebElement Login = driver.findElement(By.xpath("//*[@data-testid = 'navbar-login']"));
-        Assert.assertEquals("Login", Login.getText());
-        Login.click();
-        Assert.assertTrue(valueOfActiveElement.contains("nav-link active"));
-        driver.quit();
+            WebElement Practice = driver.findElement(By.xpath("//*[@data-testid = 'navbar-practice']"));
+            Assert.assertEquals("Practice", Practice.getText());
+            Practice.click();
+            Assert.assertTrue(valueOfActiveElement.contains("nav-link active"));
+
+            WebElement Login = driver.findElement(By.xpath("//*[@data-testid = 'navbar-login']"));
+            Assert.assertEquals("Login", Login.getText());
+            Login.click();
+            Assert.assertTrue(valueOfActiveElement.contains("nav-link active"));
+        } finally {
+            driver.quit();
+        }
     }
 }
