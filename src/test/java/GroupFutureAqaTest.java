84ef3366ccaf5b1036972a31593ee6cba621cf25
NataliaMiro <natalymir82@gmail.com>
2026-03-27 15:59:35 +0300

    Добавила удаленные импортируемые файлы import org.openqa.selenium.By; import org.openqa.selenium.WebDriver; import org.openqa.selenium.WebElement;

--- src/test/java/GroupFutureAqaTest.java
+++ src/test/java/GroupFutureAqaTest.java
@@ -10,6 +10,9 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.time.Duration;
 
