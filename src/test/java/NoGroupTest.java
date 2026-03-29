35bed5af1f70f1446d2bf91cf6f0877e3eb604dc
Natalia Barakova <Garradegaruda@gmail.com>
2026-03-29 11:35:46 +0300

    Main natasha (#142)
    * Delete class and move tests to NoGroupTest

--- src/test/java/NoGroupTest.java
+++ src/test/java/NoGroupTest.java
@@ -367,4 +367,39 @@ public class NoGroupTest {
             driver.quit();
         }
     }
+    @Test
+    public void testFirst() {
+
+        WebDriver driver = new ChromeDriver();
+        try{
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        driver.findElement(By.cssSelector("button")).click();
+
+        WebElement message = driver.findElement(By.id("message"));
+
+        Assert.assertEquals(message.getText(), "Received!");
+        }finally {
+        driver.quit();
+        }
+    }
+
+    @Test
+    public void testSecond() {
+
+        WebDriver driver = new ChromeDriver();
+        try{
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement mainTitle = driver.findElement(By.xpath("//h1[@class='display-6']"));
+
+        Assert.assertEquals(mainTitle.getText(),"Web form");
+        }finally {
+            driver.quit();
+        }
+    }
 }
