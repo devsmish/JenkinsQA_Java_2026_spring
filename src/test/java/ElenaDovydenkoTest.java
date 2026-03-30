7a90be0e3f27a98de8f6b1bd05e081b51bfe700c
ElenaDovydenko <bchild1985@gmail.com>
2026-03-30 17:02:32 +0300

    Edit class

--- src/test/java/ElenaDovydenkoTest.java
+++ src/test/java/ElenaDovydenkoTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -9,23 +10,21 @@ import java.time.Duration;
 public class ElenaDovydenkoTest {
         @Test
     public void testA1(){
+
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.a1.by/ru/");
 
-        driver.getTitle();
-
-       
-  
-        WebElement submitButton1 = driver.findElement(By.xpath("//*[@id=\"CookiesStickyPanel\"]/div[1]/div[2]/div[2]/button[2]"));
+        WebElement submitButton1 = driver.findElement(By.xpath("//*[@id='CookiesStickyPanel']/div[1]/div[2]/div[2]/button[2]"));
         submitButton1.click();
-        WebElement submitButton2 = driver.findElement(By.xpath("//a[@href='https://www.a1.by/ru/services/other-services/perehod-na-a1/p/perehod_na_a1']"));
 
-   
+        WebElement submitButton2 = driver.findElement(By.xpath("//a[@href='https://www.a1.by/ru/services/other-services/perehod-na-a1/p/perehod_na_a1']"));
         submitButton2.click();
 
-        WebElement message = driver.findElement(By.xpath("//*[@id=\"page-content\"]/div[4]/div/div/div/div/div/h3"));
+        WebElement message = driver.findElement(By.xpath("//*[@id='page-content']/div[4]/div/div/div/div/div/h3"));
         message.getText();
+
+        Assert.assertEquals(message.getText(), "Преимущества переноса номера в А1");
 
         driver.quit();
     }
