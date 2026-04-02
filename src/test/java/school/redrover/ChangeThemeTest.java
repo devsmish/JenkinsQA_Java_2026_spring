727b71d76692cbed1bd0edbc653ad1d2128386ea
Yakov Tereshenkov <tereshenkov@gmail.com>
2026-04-03 01:34:36 +0300

    add waituntil to fix ElementClickIntercepted

--- src/test/java/school/redrover/ChangeThemeTest.java
+++ src/test/java/school/redrover/ChangeThemeTest.java
@@ -26,8 +26,9 @@ public class ChangeThemeTest extends BaseTest {
         );
 
         getDriver().findElement(By.xpath("//label[@for='radio-block-0']")).click();
-        getDriver().findElement
-                (By.xpath("//button[@class='jenkins-button apply-button']")).click();
 
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//button[@class='jenkins-button apply-button']"))).click();
     }
 }
