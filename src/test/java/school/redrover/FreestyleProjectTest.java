fb3d027d876f23fbd77116c530ad68136e3c19d9
Anastasia Bodunova <anprado21@gmail.com>
2026-04-02 21:11:29 +0200

    public void testCreateFreestyleProject() { add test FrestyleProject Enable/disable project and add WebDriverWait to the testCreateFreestyleProject

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -19,7 +19,11 @@ public class FreestyleProjectTest extends BaseTest {
         getDriver().findElement(By.id("name")).sendKeys(testProjectName);
         getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
         getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.xpath("//a[@class='app-jenkins-logo']")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[@class='app-jenkins-logo']"))).click();
 
         Assert.assertEquals(getDriver().findElement(
                 By.xpath("//*[@class='jenkins-table__link model-link inside']")).getText(),
