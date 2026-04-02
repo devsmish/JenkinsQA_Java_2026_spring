d9c45caba12c90bcc5e8a5bfa4ae4203e836ac81
Elena <e.lena.galina@yandex.ru>
2026-04-02 13:45:55 +0300

    add test for create Pipeline

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -2,9 +2,13 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
+import java.time.Duration;
 
 public class PipelineTest extends BaseTest {
 
@@ -16,10 +20,11 @@ public class PipelineTest extends BaseTest {
         getDriver().findElement(By.id("name")).sendKeys(projectName);
         getDriver().findElement(By.xpath("//span[text()='Pipeline']")).click();
         getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.className("app-jenkins-logo")).click();
 
-        WebElement actualProjectName = getDriver().findElement(By
-                .xpath("//span[text()='%s']".formatted(projectName)));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("app-jenkins-logo"))).click();
+
+        WebElement actualProjectName = getDriver().findElement(By.xpath("//span[text()='%s']".formatted(projectName)));
         Assert.assertEquals(actualProjectName.getText(), projectName);
     }
 }
