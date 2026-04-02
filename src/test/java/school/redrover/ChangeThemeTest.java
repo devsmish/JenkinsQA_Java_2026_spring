2f19c375d7b1e730f0ec573764383054d284b552
Lidiya Shakirova <your_email@example.com>
2026-04-02 23:20:21 +0300

    change test

--- src/test/java/school/redrover/ChangeThemeTest.java
+++ src/test/java/school/redrover/ChangeThemeTest.java
@@ -19,12 +19,12 @@ public class ChangeThemeTest extends BaseTest {
     public void testChangeDarkTheme(){
 
         getDriver().findElement(By.xpath("//*[@id='root-action-UserAction']")).click();
-        getDriver().findElement(By.xpath("/#tasks > div:nth-child(5)")).click();
+        getDriver().findElement(By.xpath("//a[@href='/user/admin/appearance']")).click();
         getDriver().findElement(By.xpath("//label[@for='radio-block-1']")).click();
         getDriver().findElement
                 (By.xpath("//button[@class='jenkins-button jenkins-submit-button jenkins-button--primary ']")).click();
 
-
+        
 
         Assert.assertEquals("dark",
                 ((JavascriptExecutor) getDriver()).executeScript("return document.documentElement.getAttribute('data-theme')")
