3fc6591e201942f170ac14f61d1f30e65bc18e84
Anastasia Bodunova <anprado21@gmail.com>
2026-04-02 20:39:59 +0200

    add test FrestyleProject Enable/disable project

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -1,9 +1,13 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
+import java.time.Duration;
 
 public class FreestyleProjectTest extends BaseTest {
 
@@ -20,5 +24,40 @@ public class FreestyleProjectTest extends BaseTest {
         Assert.assertEquals(getDriver().findElement(
                 By.xpath("//*[@class='jenkins-table__link model-link inside']")).getText(),
                 testProjectName);
+    }
+
+    @Test
+    public void testDisableFreestyleProject() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("FreestyleProject");
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//label[@class='jenkins-toggle-switch__label ']")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        Assert.assertTrue(getDriver().findElement(
+                By.id("enable-project")).getText().contains("This project is currently disabled"));
+    }
+
+    @Test
+    public void testEnableFreestyleProject() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("FreestyleProject");
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//label[@class='jenkins-toggle-switch__label ']")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.xpath("//button[@value='Enable']")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[@href='/job/FreestyleProject/configure']"))).click();
+
+        Assert.assertEquals(getDriver().findElement(
+                By.className("jenkins-toggle-switch__label__checked-title")).getText(),
+                "Enabled");
     }
 }
