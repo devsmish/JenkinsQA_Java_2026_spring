039adf4e0dff2a060aa0e42172760ff552613751
Olga <khairutdinova.oi@gmail.com>
2026-04-03 17:19:24 +0300

    изменила ранее запушенный тест разбила на два теста для текста сообщения (убрала переменные, сделала другой XPath) и цвета сообщения удалила старый класс

--- src/test/java/school/redrover/SignInPageAlertTextColorTest.java
+++ src/test/java/school/redrover/SignInPageAlertTextColorTest.java
@@ -5,40 +5,28 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.common.JenkinsUtils;
 
 import java.time.Duration;
 
-public class SignInPageAlertMessageTextAndColorTest extends BaseTest {
-    @Ignore
+public class SignInPageAlertTextColorTest extends BaseTest {
     @Test
-    public void testSignInPageAlertMessageTextAndColor (){
+    public void testSignInPageAlertTextColor (){
         JenkinsUtils.logout(getDriver());
 
+        getDriver().findElement(By.cssSelector("#j_username")).sendKeys("user");
 
-        WebElement inputUserName = getDriver().findElement(By.cssSelector("#j_username"));
-        inputUserName.sendKeys("user");
+        getDriver().findElement(By.cssSelector("#j_password")).sendKeys("qwerty");
 
-        WebElement password = getDriver().findElement(By.cssSelector("#j_password"));
-        password.sendKeys("qwerty");
-
-        WebElement signButton = getDriver().findElement(By.xpath("//button[text()='Sign in']"));
-        signButton.click();
-
+        getDriver().findElement(By.xpath("//button[text()='Sign in']")).click();
 
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
         WebElement alertText = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//div[text()='Invalid username or password']")));
 
-        Assert.assertEquals(alertText.getText(), "Invalid username or password");
-
-
         String actualColor = alertText.getCssValue("color");
         Assert.assertTrue(actualColor.contains("oklch(0.6 0.2671 30)"),
                 "Цвет текста ошибки не красный: " + actualColor);
-
-    }
-}
+}}
