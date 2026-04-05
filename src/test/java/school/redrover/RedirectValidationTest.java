b9acf203e7af2a4eed2487d4473d36c3ca366144
Evgen Batsanov <evgenbatsanov@gmail.com>
2026-04-05 18:50:36 +0300

    Added redirect and validation test.

---
+++ src/test/java/school/redrover/RedirectValidationTest.java
@@ -0,0 +1,40 @@
+package school.redrover;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+
+public class RedirectValidationTest extends BaseTest {
+    @Test
+    public void testCheckRedirect() {
+        getDriver().findElement(By.xpath("//*[@class='task '][1]")).click();
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//h1")).getText(),
+                "New Item");
+    }
+
+    @Test
+    public void testCheckMessageEnabled() {
+        getDriver().findElement(By.xpath("//*[@class='task '][1]")).click();
+        getDriver().findElement(By.xpath("//h1")).click();
+        WebElement validation = getDriver().findElement(By.xpath("//div[@id='itemname-required']"));
+        Assert.assertFalse(
+                validation.getAttribute("class").contains("input-message-disabled")
+        );
+    }
+
+    @Test
+    public void testCheckMessageDisabled() {
+        getDriver().findElement(By.xpath("//*[@class='task '][1]")).click();
+        getDriver().findElement(By.xpath("//h1")).click();
+        WebElement validation = getDriver().findElement(By.xpath("//div[@id='itemname-required']"));
+        WebElement element = getDriver().findElement(By.xpath("//input[@id='name']"));
+        element.sendKeys("Абракадабра");
+        Assert.assertTrue(
+                validation.getAttribute("class").
+                        contains("input-message-disabled")
+        );
+    }
+}
