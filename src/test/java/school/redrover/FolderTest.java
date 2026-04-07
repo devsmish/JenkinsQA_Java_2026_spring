9d22b6cacd517870e2781a4a92d53cf20614a86c
NataliaMiro <natalymir82@gmail.com>
2026-04-07 12:34:09 +0300

    Создала класс FolderTest. Написала метод для создания New Folder. Добавила 2 теста - проверка отображения видимости секции Health Metrics при создании и изменении конфигурации.

---
+++ src/test/java/school/redrover/FolderTest.java
@@ -0,0 +1,39 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class FolderTest extends BaseTest {
+
+    public void createFolder() {
+        String folderName = "Test_Folder_HealthMetrics";
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(folderName);
+        getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+    }
+    @Test
+    public void testHealthMetricsAvailableOnFolderCreation() {
+        createFolder();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//div[@id='health-metrics']")).getText(),
+                "Health metrics",
+                "Health metrics section text should match expected");
+    }
+
+    @Test
+    public void testHealthMetricsAvailableOnFolderChanges() {
+        createFolder();
+        getDriver().findElement(By.xpath("//span[text()='Jenkins']")).click();
+        getDriver().findElement(By.xpath("//button[@class='jenkins-menu-dropdown-chevron']")).click();
+        getDriver().findElement(By.cssSelector("#tippy-4 > div > div > div > a:nth-child(1)")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//div[@id='health-metrics']")).getText(),
+                "Health metrics",
+                "Health metrics section text should match expected");
+    }
+}
