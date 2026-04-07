904f4237f2117ccf667c7f314bc2e2c5763c8359
NataliaMiro <natalymir82@gmail.com>
2026-04-07 13:22:18 +0300

    Исправила, 1 тест - 1 тест кейс

--- src/test/java/school/redrover/FolderTest.java
+++ src/test/java/school/redrover/FolderTest.java
@@ -11,17 +11,6 @@ import java.time.Duration;
 
 public class FolderTest extends BaseTest {
 
-    public void createFolder() {
-        String folderName = "Test_Folder_HealthMetrics";
-        getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(folderName);
-        getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-
-        new WebDriverWait(getDriver(), Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("health-metrics")));
-
-    }
     @Test
     public void testHealthMetricsAvailableOnFolderCreation() {
         String folderName = "Test_Folder_HealthMetrics";
