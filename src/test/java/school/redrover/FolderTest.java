65476d668e5eba0cb817857f6b4e98b600cad20b
NataliaMiro <natalymir82@gmail.com>
2026-04-07 13:16:56 +0300

    Исправила, 1 тест - 1 тест кейс

--- src/test/java/school/redrover/FolderTest.java
+++ src/test/java/school/redrover/FolderTest.java
@@ -24,20 +24,14 @@ public class FolderTest extends BaseTest {
     }
     @Test
     public void testHealthMetricsAvailableOnFolderCreation() {
-        createFolder();
+        String folderName = "Test_Folder_HealthMetrics";
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(folderName);
+        getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
 
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//div[@id='health-metrics']")).getText(),
-                "Health metrics",
-                "Health metrics section text should match expected");
-    }
-
-    @Test
-    public void testHealthMetricsAvailableOnFolderChanges() {
-        createFolder();
-        getDriver().findElement(By.xpath("//span[text()='Jenkins']")).click();
-        getDriver().findElement(By.xpath("//button[@class='jenkins-menu-dropdown-chevron']")).click();
-        getDriver().findElement(By.cssSelector("#tippy-4 > div > div > div > a:nth-child(1)")).click();
+        new WebDriverWait(getDriver(), Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("health-metrics")));
 
         Assert.assertEquals(
                 getDriver().findElement(By.xpath("//div[@id='health-metrics']")).getText(),
