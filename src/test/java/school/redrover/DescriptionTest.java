96995010f28a0ffefb5eaffbf6f453f5ee9a1afc
TanyaTD24 <td.lar@ya.ru>
2026-04-08 09:45:02 +0300

    ignore DescriptionTest

--- src/test/java/school/redrover/DescriptionTest.java
+++ src/test/java/school/redrover/DescriptionTest.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
 import java.time.Duration;
 
+@Ignore
 public class DescriptionTest extends BaseTest {
 
     @Test
