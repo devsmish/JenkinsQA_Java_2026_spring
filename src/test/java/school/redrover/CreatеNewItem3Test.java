3a6869911bc004d0da60ab238236bd891b7db87a
NataliaMiro <natalymir82@gmail.com>
2026-04-08 17:40:18 +0300

    Переименовала класс теста для привязки к US TC, изменила название теста

--- src/test/java/school/redrover/CreatеNewItem3Test.java
+++ src/test/java/school/redrover/CreatеNewItem3Test.java
@@ -5,9 +5,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-public class EmptyNameErrorMessageTest extends BaseTest {
+public class CreatеNewItem3Test extends BaseTest {
+
     @Test
-    public void testEmptyNameErrorMessage() {
+    public void testValidationErrorForEmptyNameWhenSelectingPipeline() {
         getDriver().findElement(By.linkText("New Item")).click();
         getDriver().findElement(By.xpath("//span[text()='Pipeline']")).click();
 
