73957a3ec5de676318b385a5c141f6ff69b03d5f
Olga <khairutdinova.oi@gmail.com>
2026-04-09 16:15:43 +0300

    Добавила в класс SingOut2Test.java для US_08.002 еще один тест проверку, что поле Пароль пустое после разлогинивания

--- src/test/java/school/redrover/SingOut2Test.java
+++ src/test/java/school/redrover/SingOut2Test.java
@@ -12,103 +12,75 @@ import school.redrover.common.BaseTest;
 
 public class SingOut2Test extends BaseTest {
 
-    private boolean isAlertPresent(WebDriver driver) {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
+        private boolean isAlertPresent(WebDriver driver) {
+            try {
+                driver.switchTo().alert();
+                return true;
+            } catch (NoAlertPresentException e) {
+                return false;
+            }
+        }
+
+        @Test
+        public void testSingOutIsImmediate() {
+            WebElement userButton = getDriver().findElement(By.id("root-action-UserAction"));
+
+            Actions actions = new Actions(getDriver());
+            actions.moveToElement(userButton).perform();
+
+            WebElement dropdownMenu = getWait5().until(
+                    ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-dropdown']"))
+            );
+            dropdownMenu.findElement(By.xpath(".//a[@href='/logout']")).click();
+
+            Assert.assertFalse(isAlertPresent(getDriver()),
+                    "Не должно быть alert-окна подтверждения выхода. Выход должен быть мгновенным.");
+
+            getWait5().until(ExpectedConditions.urlContains("login"));
+        }
+
+        @Test
+        public void testJenkinsSingOutButtonUserNameEmpty() {
+            WebElement userButton = getDriver().findElement(By.id("root-action-UserAction"));
+
+            Actions actions = new Actions(getDriver());
+            actions.moveToElement(userButton).perform();
+
+            WebElement dropdownMenu = getWait5().until(
+                    ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-dropdown']"))
+            );
+            dropdownMenu.findElement(By.xpath(".//a[@href='/logout']")).click();
+
+            getWait5().until(
+                    ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']"))
+            );
+
+            WebElement usernameField = getDriver().findElement(By.id("j_username"));
+            String usernameValue = usernameField.getAttribute("value");
+            Assert.assertEquals(usernameValue, "",
+                    "Поле 'Username' должно быть пустым, но содержит: '" + usernameValue + "'");
+        }
+
+        @Test
+        public void testJenkinsSingOutButtonPasswordEmpty() {
+
+            WebElement userButton = getDriver().findElement(By.id("root-action-UserAction"));
+
+            Actions actions = new Actions(getDriver());
+            actions.moveToElement(userButton).perform();
+
+            WebElement dropdownMenu = getWait5().until(
+                    ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-dropdown']"))
+            );
+            dropdownMenu.findElement(By.xpath(".//a[@href='/logout']")).click();
+
+            getWait5().until(
+                    ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']"))
+            );
+
+            WebElement passwordField = getDriver().findElement(By.id("j_password"));
+            String passwordValue = passwordField.getAttribute("value");
+            Assert.assertEquals(passwordValue, "",
+                    "Поле 'Password' должно быть пустым, но содержит: '" + passwordValue + "'");
         }
     }
-
-    @Test
-    public void testSingOutIsImmediate() {
-        WebElement userButton = getDriver().findElement(By.id("root-action-UserAction"));
-
-        Actions actions = new Actions(getDriver());
-        actions.moveToElement(userButton).perform();
-
-        WebElement dropdownMenu = getWait5().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-dropdown']"))
-        );
-        dropdownMenu.findElement(By.xpath(".//a[@href='/logout']")).click();
-
-        Assert.assertFalse(isAlertPresent(getDriver()),
-                "Не должно быть alert-окна подтверждения выхода. Выход должен быть мгновенным.");
-
-        getWait5().until(ExpectedConditions.urlContains("login"));
-    }
-
-    @Test
-    public void testJenkinsSingOutButtonUserNameEmpty() {
-        WebElement userButton = getDriver().findElement(By.id("root-action-UserAction"));
-
-        Actions actions = new Actions(getDriver());
-        actions.moveToElement(userButton).perform();
-
-        WebElement dropdownMenu = getWait5().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-dropdown']"))
-        );
-        dropdownMenu.findElement(By.xpath(".//a[@href='/logout']")).click();
-
-        getWait5().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']"))
-        );
-
-        WebElement usernameField = getDriver().findElement(By.id("j_username"));
-        String usernameValue = usernameField.getAttribute("value");
-        Assert.assertEquals(usernameValue, "",
-                "Поле 'Username' должно быть пустым, но содержит: '" + usernameValue + "'");
-    }
-
-    @Test
-    public void testJenkinsSingOutButtonPasswordEmpty() {
-
-        WebElement userButton = getDriver().findElement(By.id("root-action-UserAction"));
-
-        Actions actions = new Actions(getDriver());
-        actions.moveToElement(userButton).perform();
-
-
-        WebElement dropdownMenu = getWait5().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-dropdown']"))
-        );
-        dropdownMenu.findElement(By.xpath(".//a[@href='/logout']")).click();
-
-
-        getWait5().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']"))
-        );
-
-        WebElement passwordField = getDriver().findElement(By.id("j_password"));
-        String passwordValue = passwordField.getAttribute("value");
-        Assert.assertEquals(passwordValue, "",
-                "Поле 'Password' должно быть пустым, но содержит: '" + passwordValue + "'");
-    }
-
-    @Test
-    public void testJenkinsSingOutButtonPasswordEmpty() {
-
-        WebElement userButton = getDriver().findElement(By.id("root-action-UserAction"));
-
-        Actions actions = new Actions(getDriver());
-        actions.moveToElement(userButton).perform();
-
-
-        WebElement dropdownMenu = getWait5().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-dropdown']"))
-        );
-        dropdownMenu.findElement(By.xpath(".//a[@href='/logout']")).click();
-
-
-        getWait5().until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']"))
-        );
-
-        WebElement passwordField = getDriver().findElement(By.id("j_password"));
-        String passwordValue = passwordField.getAttribute("value");
-        Assert.assertEquals(passwordValue, "",
-                "Поле 'Password' должно быть пустым, но содержит: '" + passwordValue + "'");
-    }
-
-}
