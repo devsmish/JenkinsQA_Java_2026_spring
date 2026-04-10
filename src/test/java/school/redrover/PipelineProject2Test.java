790f3d63528008f50f2c8dbaedfe339b52ecbd9e
Eni11-eni <enigul00@mail.ru>
2026-04-10 15:31:28 +0500

    add new pipeline test

--- src/test/java/school/redrover/PipelineProject2Test.java
+++ src/test/java/school/redrover/PipelineProject2Test.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
@@ -23,9 +22,9 @@ public class PipelineProject2Test extends BaseTest {
         getDriver().findElement(By.xpath("//span[text()='Pipeline']")).click();
         getDriver().findElement(By.id("ok-button")).click();
 
-        getDriver().findElement(By.xpath("//span[@class='jenkins-mobile-hide']")).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='jenkins-mobile-hide']"))).click();
     }
-    @Ignore
+
     @Test
     public void testCreateWithValidName() {
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
@@ -48,5 +47,19 @@ public class PipelineProject2Test extends BaseTest {
                 "» This field cannot be empty, please enter a valid name");
 
         Assert.assertFalse(isEnabled);
+    }
+
+    @Test
+    public void testCreateWithDuplicateName() {
+
+        createItemAndGoToMainPage(PROJECT_NAME);
+
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/view/all/newJob']"))).click();
+
+        getDriver().findElement(By.id("name")).sendKeys(PROJECT_NAME);
+        getDriver().findElement(By.xpath("//span[text()='Pipeline']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='itemname-invalid']")).getText(),
+                "» A job already exists with the name ‘%s’".formatted(PROJECT_NAME));
     }
 }
