b96376bc1ce62e1821980184ffc8e1d5b0efad15
dbrovkov <media2dmg@gmail.com>
2026-04-10 08:18:43 +0300

    test reworked

--- src/test/java/school/redrover/DashboardTest.java
+++ src/test/java/school/redrover/DashboardTest.java
@@ -2,26 +2,21 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-import java.time.Duration;
-@Ignore
 public class DashboardTest extends BaseTest {
 
     @Test
     void checkDescriptionAdd() {
+        String desc = "Test dashboard description here";
+
         getDriver().findElement(By.id("description-link")).click();
-        getDriver().findElement(By.name("description")).sendKeys("Test dashboard description here");
-        getDriver().findElement(By.name("Submit")).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.name("description"))).sendKeys(desc);
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
-        WebDriverWait waiting = new WebDriverWait(getDriver(), Duration.ofSeconds(3));
-        boolean descTextAppear = waiting.until(ExpectedConditions.textToBePresentInElementLocated(
-                By.id("description-content"), "Test dashboard description here"));
-
-        Assert.assertEquals(getDriver().findElement(By.id("description-content")).getText(), "Test dashboard description here");
+        getWait5().until(ExpectedConditions.textToBePresentInElementLocated(By.id("description-content"), desc));
+        Assert.assertEquals(getDriver().findElement(By.id("description-content")).getText(), desc);
     }
 }
