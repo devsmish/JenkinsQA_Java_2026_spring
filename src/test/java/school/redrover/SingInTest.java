7330b70504a1400a2432853ead1a2f7af4fec8d9
petr2o1o <150625579+petr2o1o@users.noreply.github.com>
2026-04-10 10:25:16 +0800

    create test login with invalid password (#397)
    Co-authored-by: Petr P <none@none.none>

--- src/test/java/school/redrover/SingInTest.java
+++ src/test/java/school/redrover/SingInTest.java
@@ -1,59 +1,78 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-import java.time.Duration;
 
 public class SingInTest extends BaseTest {
 
     private void logout(){
 
-        Wait<WebDriver> wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-
-        WebElement userAction = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("root-action-UserAction")));
+        WebElement userButton = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("root-action-UserAction")));
 
         Actions action = new Actions(getDriver());
-        action.moveToElement(userAction).perform();
+        action.moveToElement(userButton).perform();
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/logout']"))).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/logout']"))).click();
     }
-    @Ignore
+
     @Test
     public void testLoginValidData () {
 
         final String userLogin = "KozhemiakaN";
-        final String userPassw = "Nik123";
+        final String userPassword = "Nik123";
 
         TestUtility.createUser(userLogin,
                 "Nikita",
-                userPassw,
-                userPassw,
+                userPassword,
+                userPassword,
                 "kozhemiaka@nikita.da",
                 getDriver());
 
         logout();
 
-        Wait<WebDriver> wait = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("app-sign-in-register__content-inner")));
+
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.className("app-sign-in-register__content-inner")));
 
         getDriver().findElement(By.name("j_username")).sendKeys(userLogin);
-        getDriver().findElement(By.name("j_password")).sendKeys(userPassw);
+        getDriver().findElement(By.name("j_password")).sendKeys(userPassword);
         getDriver().findElement(By.name("Submit")).click();
 
-        WebElement header = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
+        WebElement header = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
 
         Assert.assertEquals(header.getText(), "Welcome to Jenkins!");
     }
 
+    @Test
+    public void testLoginInvalidPassword () {
+
+        final String userLogin = "Berendey";
+        final String userPassword = "Beren123";
+
+        TestUtility.createUser(userLogin,
+                "Berendey",
+                userPassword,
+                userPassword,
+                "berendey@kingdom.pz",
+                getDriver());
+
+        logout();
+
+
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.className("app-sign-in-register__content-inner")));
+
+        getDriver().findElement(By.name("j_username")).sendKeys(userLogin);
+        getDriver().findElement(By.name("j_password")).sendKeys("nik123");
+        getDriver().findElement(By.name("Submit")).click();
+
+        WebElement errorMessage = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.className("app-sign-in-register__error")));
+
+        Assert.assertEquals(errorMessage.getText(), "Invalid username or password");
+    }
 
 }
