6ca05d4d3df5a2c0ad3fda7fc6efe5faf1e5bfd6
babadza <babadza@gmail.com>
2026-04-10 12:50:38 +0300

    It was added new test for nested folder in CreateFolderTest

--- src/test/java/school/redrover/CreateFolderTest.java
+++ src/test/java/school/redrover/CreateFolderTest.java
@@ -1,11 +1,14 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class CreateFolderTest extends BaseTest {
 
@@ -31,4 +34,45 @@ public class CreateFolderTest extends BaseTest {
                 "TestFolder");
 
     }
+
+    @Test
+    public void createNestedFolderTest(){
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"))
+                .click();
+
+
+        getDriver().findElement(By.xpath("//input[@id='name']"))
+                .sendKeys("ParentFolder");
+        getDriver().findElement(By.xpath(
+                        "//li[contains(@class,'com_cloudbees_hudson_plugins_folder_Folder')]"))
+                .click();
+
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+        getDriver().findElement(By.xpath("//button[@value='Save']"))
+                .click();
+
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='app-jenkins-logo']"))).click();
+        //getDriver().findElement(By.xpath("//a[@class='app-jenkins-logo']")).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//td/a[@href='job/ParentFolder/']"))).click();
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']"))
+                .sendKeys("ChildFolder");
+        getDriver().findElement(By.xpath(
+                        "//li[contains(@class,'com_cloudbees_hudson_plugins_folder_Folder')]"))
+                .click();
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+        getDriver().findElement(By.xpath("//button[@value='Save']"))
+                .click();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//div[@id='main-panel' and contains(text(), 'Full folder name: ParentFolder/ChildFolder')]")).getText().contains("ParentFolder/ChildFolder"));
+        //(getDriver().findElement(By.xpath("//div[@id='main-panel' and contains(text(), 'Full folder name: ParentFolder/ChildFolder')]")).getText(),"ParentFolder/ChildFolder'");
+
+
+
+
+        //getDriver().findElement(By.xpath("//td/a[@href='job/ParentFolder/']")).click();
+
+    }
+
+
 }
