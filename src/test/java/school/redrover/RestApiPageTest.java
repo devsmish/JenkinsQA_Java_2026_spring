567215a0e16199d20a949a323c0f45481b5313b0
Olga <khairutdinova.oi@gmail.com>
2026-04-11 12:28:55 +0300

    Создала класс RestApiPageTest.java для US_15.002 тест на проверку того что ссылка Rest Api Скрывается на самой странице Rest Api удалила ненужный класс

--- src/test/java/school/redrover/RestApiPageTest.java
+++ src/test/java/school/redrover/RestApiPageTest.java
@@ -14,18 +14,14 @@ public class RestApiPageTest extends BaseTest {
 
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
 
-        // Прокручиваем до футера
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 
-        // Кликаем по ссылке "REST API" в футере (используем getWait10())
         getWait10().until(
                 ExpectedConditions.elementToBeClickable(By.xpath("//footer//a[contains(text(),'REST API')]"))
         ).click();
 
-        // Прокручиваем до футера на открывшейся странице
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 
-        // Проверяем, что ссылка отсутствует в футере
         boolean isRestApiLinkPresentInFooter = !getDriver().findElements(
                 By.xpath("//footer//a[contains(text(),'REST API')]")
         ).isEmpty();
