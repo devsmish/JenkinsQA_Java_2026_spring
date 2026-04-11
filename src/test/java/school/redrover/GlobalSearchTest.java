64c5ee8c23f92b42161ec1b5b2015be2bf2d8ae7
Aivengo <ansfilvel@gmail.com>
2026-04-11 20:42:24 +0300

    Add new test Change Reguest

--- src/test/java/school/redrover/GlobalSearchTest.java
+++ src/test/java/school/redrover/GlobalSearchTest.java
@@ -14,6 +14,19 @@ public class GlobalSearchTest extends BaseTest {
     private static final String TEXT_TO_SEARCH = "test12321";
 
 
+    private void createFolder(String folderName) {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(folderName);
+        getDriver().findElement(By.xpath("//li[@class='com_cloudbees_hudson_plugins_folder_Folder']")).click();
+
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+
+        getDriver().findElement(By.xpath("//a[@class='app-jenkins-logo']")).click();
+
+    }
+
+
     @Test
     public void testClearingTheSearchField(){
 
@@ -25,5 +38,25 @@ public class GlobalSearchTest extends BaseTest {
 
         Assert.assertEquals(searchInput.getAttribute("value"), "");
 
+    }
+
+    @Test
+    public void testChangeReguest(){
+
+        createFolder("FirstFolder");
+        createFolder("SecondFolder");
+
+        getDriver().findElement(SEARCH_BUTTON).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(SEARCH_INPUT_FIELD));
+        WebElement searchInput = getDriver().findElement(SEARCH_INPUT_FIELD);
+        searchInput.sendKeys("FirstFolder");
+        searchInput.clear();
+        searchInput.sendKeys("SecondFolder");
+
+        WebElement result = getWait5().until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(@href, 'SecondFolder')]"))
+        );
+        String actualText = result.getText();
+        Assert.assertTrue(actualText.contains("SecondFolder"));
     }
 }
