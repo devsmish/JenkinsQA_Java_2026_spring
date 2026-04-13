c6bdcd6fc2b2add509a553cc07bf82f6dc9caf3e
NadiaSergeeva <nadechka.ten@gmail.com>
2026-04-14 01:02:27 +0200

    TC_11.003.02 |Welcome Dashboard > Manage Jenkins > Verify search is case-insensitive

--- src/test/java/school/redrover/ManageJenkinsPage3Test.java
+++ src/test/java/school/redrover/ManageJenkinsPage3Test.java
@@ -3,12 +3,10 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-
-import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,6 +14,7 @@ public class ManageJenkinsPage3Test extends BaseTest {
 
     private static final By MANAGE_JENKINS_LINK = By.cssSelector("a[href='/manage']");
     private static final By CONFIGURE_SYSTEM_LINK = By.xpath("//a[contains(@href, 'configure')]");
+    private static final By SEARCH_BAR = By.id("settings-search-bar");
 
     @Test
     public void testOpenConfigureSystemPage() {
@@ -56,5 +55,24 @@ public class ManageJenkinsPage3Test extends BaseTest {
             Assert.assertTrue(section.isDisplayed(),
                     "Section with id '" + sectionId + "' should be displayed on Configure System page");
         }
+    }
+
+    @Test
+    public void testSearchCaseInsensitive(){
+        List<String> inputValues = List.of("system","SYSTEM","uSeRs");
+        List<String> expectedSections = List.of("System","System","Users");
+        List<String> actualSections= new ArrayList<>();
+
+        getWait10().until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS_LINK)).click();
+        getWait10().until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//h1"), "Manage Jenkins"));
+
+        for (String input:inputValues) {
+            getDriver().findElement(SEARCH_BAR).sendKeys(input);
+            actualSections.add(getDriver().findElement(By.xpath("//a[contains(@class, 'jenkins-dropdown__item')]")).getText());
+            getDriver().findElement(SEARCH_BAR).clear();
+        }
+
+        Assert.assertEquals(actualSections,expectedSections);
+
     }
 }
