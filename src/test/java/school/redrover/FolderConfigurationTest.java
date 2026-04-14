438871f2ccd60ac625a539f49586c22194c386e2
Eni11-eni <enigul00@mail.ru>
2026-04-14 22:29:49 +0500

    add new test

--- src/test/java/school/redrover/FolderConfigurationTest.java
+++ src/test/java/school/redrover/FolderConfigurationTest.java
@@ -21,15 +21,18 @@ public class FolderConfigurationTest extends BaseTest {
         getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
         getWait10().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='ok-button']"))).click();
 
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Jenkins']"))).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1")));
+        getDriver().findElement(By.xpath("//span[text()='Jenkins']")).click();
 
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='jenkins-table__link model-link inside']")));
         getWait10().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='jenkins-menu-dropdown-chevron']"))).click();
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='Configure']"))).click();
 
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='_.displayNameOrNull']"))).sendKeys(FOLDER_NAME2);
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Jenkins']"))).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1")));
+        getDriver().findElement(By.xpath("//span[text()='Jenkins']")).click();
 
         WebElement newName = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='jenkins-table__link model-link inside']")));
 
