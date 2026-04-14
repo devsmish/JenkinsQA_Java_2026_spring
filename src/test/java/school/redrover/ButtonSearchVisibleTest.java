6a39447f748221d0c5c28de74cc0a8ea7eadbbd7
ma9or <il.akhmetzhanov@yandex.kz>
2026-04-14 21:08:04 +0500

    add new test in Jenkins

---
+++ src/test/java/school/redrover/ButtonSearchVisibleTest.java
@@ -0,0 +1,21 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class ButtonSearchVisibleTest extends BaseTest {
+
+    @Test
+    public void testButtonSearch () {
+        getDriver().findElement(By.xpath("//*[@id='root-action-SearchAction']")).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='search-results']/a")));
+        getDriver().findElement(By.xpath("//*[@id='command-bar']")).sendKeys("text");
+        String expectedText = getDriver().findElement(By.xpath("//*[@id='command-bar']")).getAttribute("value");
+        Assert.assertEquals(expectedText, "text");
+
+    }
+}
