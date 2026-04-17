a0cef0a9bb3e0386515cf5dcf5a046138a8fcbd4
NataliaMiro <natalymir82@gmail.com>
2026-04-17 14:11:29 +0300

    Добавлен тест проверки, что имя проекта из поля Display Name (секция Advanced) заменяет техническое имя на главной странице

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -26,5 +27,40 @@ public class PipelineTest extends BaseTest {
 
         WebElement actualProjectName = getDriver().findElement(By.xpath("//span[text()='%s']".formatted(projectName)));
         Assert.assertEquals(actualProjectName.getText(), projectName);
+    }
+
+    @Test(dependsOnMethods = "testCreatePipeline")
+
+    public void testDisplayNameInAdvancedSectionChangesProjectNameOnDashboard() {
+
+        String displayName = "Changed Pipeline";
+
+
+        WebElement pipelineName = getWait10().until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector("#job_new\\ Pipeline > td:nth-child(3) > a")));
+        pipelineName.click();
+
+        getWait10().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[contains(@href, '/configure')]"))).click();
+
+        ((JavascriptExecutor) getDriver()).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        WebElement advancedButton = getWait10().until(
+                ExpectedConditions.elementToBeClickable(
+                        By.xpath("(//button[contains(@class, 'advancedButton')])[last()]")));
+        advancedButton.click();
+
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//input[@name='_.displayNameOrNull']"))).sendKeys(displayName);
+
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.id("jenkins-head-icon"))).click();
+
+        WebElement projectOnDashboard = getWait10().until(
+                ExpectedConditions.presenceOfElementLocated(
+                        By.xpath("//span[text()='%s']".formatted(displayName))));
+        Assert.assertEquals(projectOnDashboard.getText(), displayName,
+                "Project should be displayed with Display Name on dashboard");
     }
 }
