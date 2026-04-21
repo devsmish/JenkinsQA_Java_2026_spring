982b13082421f2bc801f1d607bc11a2b87e54e41
NataliaMiro <natalymir82@gmail.com>
2026-04-21 14:14:02 +0300

    Перенесла тесты в класс ManageJenkinsPageTest и удалила их из ManageJenkinsPage3Test

--- src/test/java/school/redrover/ManageJenkinsPage3Test.java
+++ src/test/java/school/redrover/ManageJenkinsPage3Test.java
@@ -14,55 +14,12 @@ import java.util.List;
 public class ManageJenkinsPage3Test extends BaseTest {
 
     private static final By MANAGE_JENKINS_LINK = By.cssSelector("a[href='/manage']");
-    private static final By CONFIGURE_SYSTEM_LINK = By.xpath("//a[contains(@href, 'configure')]");
     private static final By SEARCH_BAR = By.id("settings-search-bar");
     private static final By EMPTY_DROPDOWN = By.className("jenkins-search__results__no-results-label");
     private static final By HEADER = By.xpath("//h1");
 
     private String getHeader() {
         return getDriver().findElement(HEADER).getText();
-    }
-
-    @Test
-    public void testOpenConfigureSystemPage() {
-
-        WebElement manageJenkinsIcon = getWait10()
-                .until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS_LINK));
-        manageJenkinsIcon.click();
-
-        getWait10().until(ExpectedConditions.urlContains("/manage"));
-
-
-        WebElement configureSystemLink = getWait5()
-                .until(ExpectedConditions.elementToBeClickable(CONFIGURE_SYSTEM_LINK));
-        configureSystemLink.click();
-
-
-        getWait2().until(ExpectedConditions.urlContains("/configure"));
-        Assert.assertTrue(getDriver().getCurrentUrl().contains("/configure"),
-                "User should be redirected to Configure System page");
-    }
-
-    @Test
-    public void testConfigureSystemPageSectionsWithFields() {
-
-        getWait10().until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS_LINK)).click();
-        getWait10().until(ExpectedConditions.urlContains("/manage"));
-
-        getWait10().until(ExpectedConditions.elementToBeClickable(CONFIGURE_SYSTEM_LINK)).click();
-        getWait10().until(ExpectedConditions.urlContains("/configure"));
-
-        List<String> expectedSections = Arrays.asList(
-                "general",
-                "jenkins-location",
-                "global-properties"
-        );
-
-        for (String sectionId : expectedSections) {
-            WebElement section = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id(sectionId)));
-            Assert.assertTrue(section.isDisplayed(),
-                    "Section with id '" + sectionId + "' should be displayed on Configure System page");
-        }
     }
 
     @DataProvider
--- src/test/java/school/redrover/ManageJenkinsPageTest.java
+++ src/test/java/school/redrover/ManageJenkinsPageTest.java
@@ -2,14 +2,22 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ManageJenkinsPageTest extends BaseTest {
+
+    private static final By MANAGE_JENKINS_LINK = By.cssSelector("a[href='/manage']");
+    private static final By CONFIGURE_SYSTEM_LINK = By.xpath("//a[contains(@href, 'configure')]");
+    private static final By SEARCH_BAR = By.id("settings-search-bar");
+    private static final By EMPTY_DROPDOWN = By.className("jenkins-search__results__no-results-label");
 
     private final List<String> expectedItems = List.of("System", "Tools", "Plugins", "Nodes", "Clouds",
             "Appearance", "Security", "Credentials", "Credential Providers", "Users", "System Information",
@@ -31,4 +39,89 @@ public class ManageJenkinsPageTest extends BaseTest {
         Assert.assertEquals(actualItems, expectedItems);
     }
 
+    @DataProvider
+    public Object[][] caseInSensitive() {
+        return new Object[][]{
+                {"system", "System"},
+                {"SYSTEM", "System"},
+                {"uSeRs", "Users"}
+        };
+    }
+
+    @Test(dataProvider = "caseInSensitive")
+    public void testSearchCaseInsensitive(String input, String expOutput) {
+
+        getWait10().until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS_LINK)).click();
+        getWait10().until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//h1"), "Manage Jenkins"));
+
+        WebElement searchBar = getDriver().findElement(SEARCH_BAR);
+        searchBar.sendKeys(input);
+
+        String actualOutput = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(@class, 'jenkins-dropdown__item')]"))).getText();
+
+        Assert.assertEquals(actualOutput, expOutput);
+
+    }
+
+    @DataProvider
+    public Object[][] invalidInput() {
+        return new Object[][]{
+                {"qwerty123", "No results"},
+                {"!@#$", "No results"},
+                {"  ", "No results"}
+        };
+    }
+
+    @Test(dataProvider = "invalidInput")
+    public void testSearchInvalid(String input, String expOutput) {
+        getWait10().until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS_LINK)).click();
+        getWait10().until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//h1"), "Manage Jenkins"));
+
+        getDriver().findElement(SEARCH_BAR).sendKeys(input);
+        String actualOutput = getWait10().until(ExpectedConditions.visibilityOfElementLocated(EMPTY_DROPDOWN)).getText();
+
+        Assert.assertEquals(actualOutput, expOutput);
+    }
+
+    @Test
+    public void testOpenConfigureSystemPage() {
+
+        WebElement manageJenkinsIcon = getWait10()
+                .until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS_LINK));
+        manageJenkinsIcon.click();
+
+        getWait10().until(ExpectedConditions.urlContains("/manage"));
+
+
+        WebElement configureSystemLink = getWait5()
+                .until(ExpectedConditions.elementToBeClickable(CONFIGURE_SYSTEM_LINK));
+        configureSystemLink.click();
+
+
+        getWait2().until(ExpectedConditions.urlContains("/configure"));
+        Assert.assertTrue(getDriver().getCurrentUrl().contains("/configure"),
+                "User should be redirected to Configure System page");
+    }
+
+    @Test
+    public void testConfigureSystemPageSectionsWithFields() {
+
+        getWait10().until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS_LINK)).click();
+        getWait10().until(ExpectedConditions.urlContains("/manage"));
+
+        getWait10().until(ExpectedConditions.elementToBeClickable(CONFIGURE_SYSTEM_LINK)).click();
+        getWait10().until(ExpectedConditions.urlContains("/configure"));
+
+        List<String> expectedSections = Arrays.asList(
+                "general",
+                "jenkins-location",
+                "global-properties"
+        );
+
+        for (String sectionId : expectedSections) {
+            WebElement section = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id(sectionId)));
+            Assert.assertTrue(section.isDisplayed(),
+                    "Section with id '" + sectionId + "' should be displayed on Configure System page");
+        }
+    }
 }
