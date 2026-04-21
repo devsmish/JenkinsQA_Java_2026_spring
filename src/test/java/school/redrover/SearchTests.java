170bd049426583fa435918213cef0d589c743725
vida65979-arch <vida65979@gmai.com>
2026-04-21 20:49:24 +0600

    added new test

--- src/test/java/school/redrover/SearchTests.java
+++ src/test/java/school/redrover/SearchTests.java
@@ -11,6 +11,7 @@ import school.redrover.common.BaseTest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class SearchTests extends BaseTest {
 
@@ -21,7 +22,7 @@ public class SearchTests extends BaseTest {
 
     public static final By NEW_ITEM_BUTTON = By.xpath("(//div[@id='tasks']//a)[1]");
     public static final By NEW_ITEM_TITLE = By.xpath("//input[@id='name']");
-    public static final By PIPLINE_TYPE = By.xpath("//span[text()='Pipeline']");
+    public static final By PIPELINE_TYPE = By.xpath("//span[text()='Pipeline']");
     public static final By FREESTYLE_TYPE = By.xpath("//span[text()='Freestyle project']");
     public static final By MULTI_CONFIGURATION_TYPE = By.xpath("//span[text()='Multi-configuration project']");
     public static final By FOLDER = By.xpath("//span[text()='Folder']");
@@ -33,23 +34,30 @@ public class SearchTests extends BaseTest {
 
     private static final By SAVE_BUTTON = By.xpath("//button[@name='Submit']");
     private static final By APPLY_BUTTON = By.xpath("//button[@name='Apply']");
+    private static final By SWITCH_TOGGLE_BUTTON = By.xpath("//span[@class='jenkins-toggle-switch__label__checked-title']");
     private static final By NOTIFICATION_BAR = By.xpath("//div[@id='notification-bar']");
     private static final By DESCRIPTION_TEXTAREA = By.xpath("//textarea[@name='description']");
     private static final By JOB_DESCRIPTION =  By.xpath("//div[@id='description-content']");
 
-    private static final String JOB_NAME =  "JobTitles";
+    private static final By WARNING_FORM =  By.xpath("//form[@id='enable-project']");
+    private static final By ENABLE_BUTTON =  By.xpath("//button[@formnovalidate='formNoValidate']");
+    private static final By CREATED_JOB_NAME =  By.xpath("//div[@class='jenkins-app-bar__content jenkins-build-caption']");
+
+    private static final String JOB_NAME =  "JobTitle";
+
     public void createJob(Map<String, By> jobTitles){
 
         for (Map.Entry<String, By> entry : jobTitles.entrySet()) {
-            getWait2().until(ExpectedConditions.elementToBeClickable(NEW_ITEM_BUTTON)).click();
+            click(NEW_ITEM_BUTTON);
 
-            getDriver().findElement(NEW_ITEM_TITLE).sendKeys(entry.getKey());
-            getDriver().findElement(entry.getValue()).click();
-            getDriver().findElement(NEW_ITEM_OK_BUTTON).click();
+            fill(NEW_ITEM_TITLE, entry.getKey());
+            click(entry.getValue());
+            click(NEW_ITEM_OK_BUTTON);
 
-            getWait2().until(ExpectedConditions.urlContains("/job/"));
-            getDriver().findElement(NEW_ITEM_SAVE_BUTTON).click();
-            getWait2().until(ExpectedConditions.elementToBeClickable(JENKINS_LOGO)).click();
+            click(NEW_ITEM_SAVE_BUTTON);
+            isVisible(CREATED_JOB_NAME);
+            click(JENKINS_LOGO);
+            isClickable(NEW_ITEM_BUTTON);
         }
     }
 
@@ -59,30 +67,38 @@ public class SearchTests extends BaseTest {
         getWait2().until(ExpectedConditions.urlToBe(url));
     }
 
-    public void click(By target){
-        getWait2().until(ExpectedConditions.elementToBeClickable(target)).click();
+    public void click(By target){getWait5().until(ExpectedConditions.elementToBeClickable(target)).click();}
+
+    public void fill(By target, String input){ getDriver().findElement(target).sendKeys(input);}
+
+    public void fill(By target, Keys input){ getDriver().findElement(target).sendKeys(input);}
+
+    public void isClickable(By target){ getWait2().until(ExpectedConditions.elementToBeClickable(target));}
+
+    public void isVisible(By target){ getWait2().until(ExpectedConditions.visibilityOfElementLocated(target));}
+
+    public boolean verifyText(By target, String text){ return getWait2().until(ExpectedConditions.textToBePresentInElementLocated(target, text));}
+
+    public void getConfigurePageOf(String jobTitle){
+        getDriver().get(getDriver().getCurrentUrl() + "job/" + jobTitle + "/configure");
+        isVisible(JENKINS_LOGO);
     }
 
-    public void fill(By target, String input){
-        getDriver().findElement(target).sendKeys(input);
-    }
-    public void fill(By target, Keys input){
-        getDriver().findElement(target).sendKeys(input);
-    }
-
-    public void isClickable(By target){
-        getWait2().until(ExpectedConditions.elementToBeClickable(target));
-    }
-
-    public void isVisible(By target){
-        getWait2().until(ExpectedConditions.visibilityOfElementLocated(target));
+    public String getRandomName(){
+        char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+        Random random = new Random();
+        String name = "";
+        for(int i = 0; i < 10; i++){
+            name += chars[random.nextInt(chars.length)];
+        }
+        return name;
     }
 
     @Test
     public void testEnteringPartialWords(){
         Map<String, By> jobTitles = new HashMap<>(Map.of(
-                "Partialtest", PIPLINE_TYPE,
-                "Parttaltest", PIPLINE_TYPE));
+                "Partialtest", PIPELINE_TYPE,
+                "Parttaltest", PIPELINE_TYPE));
         createJob(jobTitles);
 
         click(SEARCH_BUTTON);
@@ -126,10 +142,10 @@ public class SearchTests extends BaseTest {
 
     @Test
     public void testApply(){
-        Map<String, By> jobTitles = new HashMap<>(Map.of(JOB_NAME, PIPLINE_TYPE));
+        Map<String, By> jobTitles = new HashMap<>(Map.of(JOB_NAME, PIPELINE_TYPE));
         createJob(jobTitles);
 
-        getDriver().get(getDriver().getCurrentUrl() + "job/" + JOB_NAME + "/configure");
+        getConfigurePageOf(JOB_NAME);
         isClickable(SAVE_BUTTON);
         fill(DESCRIPTION_TEXTAREA, "test");
         click(APPLY_BUTTON);
@@ -143,12 +159,27 @@ public class SearchTests extends BaseTest {
 
     @Test(dependsOnMethods = "testApply")
     public void testSave(){
-        getDriver().get(getDriver().getCurrentUrl() + "job/" + JOB_NAME + "/configure");
+        getConfigurePageOf(JOB_NAME);
         fill(DESCRIPTION_TEXTAREA, " text");
         click(SAVE_BUTTON);
         isVisible(JOB_DESCRIPTION);
 
         Assert.assertEquals(getDriver().findElement(JOB_DESCRIPTION).getText(), "test text");
         Assert.assertTrue(getDriver().getCurrentUrl().contains("job/" + JOB_NAME));
+    }
+
+    @Test
+    public void testWarningIfDisabled(){
+        String jobTitle = getRandomName();
+        createJob(Map.of(jobTitle, MULTI_CONFIGURATION_TYPE));
+        getConfigurePageOf(jobTitle);;
+
+        click(SWITCH_TOGGLE_BUTTON);
+        click(SAVE_BUTTON);
+        verifyText(WARNING_FORM, "This project is currently disabled");
+        click(ENABLE_BUTTON);
+
+        isVisible(JENKINS_LOGO);
+        Assert.assertTrue(getDriver().findElements(WARNING_FORM).isEmpty());
     }
 }
