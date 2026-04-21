73be4c94e089d1444deda578222b1660e99cb38b
Elena <e.lena.galina@yandex.ru>
2026-04-21 11:56:45 +0300

    AT_10.017.07 | Manage Jenkins > Users > Create user with an incorrect confirm password, and RF testCreateUserWithDuplicateUsername(), testCreateUser()

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -4,12 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-
-import java.time.Duration;
 
 public class PipelineTest extends BaseTest {
 
