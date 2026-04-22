4a56e50e079cff12f83d63db1b3ee647a59fb841
Pitstop-git <liudmilak@yandex.ru>
2026-04-22 18:19:49 +0300

    Bug fix for testMarkNodeOffline

--- src/test/java/school/redrover/NodeTest.java
+++ src/test/java/school/redrover/NodeTest.java
@@ -75,10 +75,9 @@ public class NodeTest extends BaseTest {
         getDriver().findElement(By.xpath("//a[@href='../computer/%s/']"
                 .formatted(NEW_NODE_NAME.replace(" ", "%20")))).click();
 
+        getDriver().findElement(By.xpath("//form [@action='markOffline']")).click();
         getWait5().until(ExpectedConditions.elementToBeClickable(
-                getDriver().findElement(By.xpath("//form [@action='markOffline']")))).click();
-        getWait5().until(ExpectedConditions.elementToBeClickable(
-                getDriver().findElement(By.xpath("//button [@name='Submit']")))).click();
+                getDriver().findElement(By.className("jenkins-submit-button")))).click();
 
         Assert.assertEquals(getDriver().findElement(By.className("message")).getText(), "Disconnected by admin");
     }
